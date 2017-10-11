de811ed548453319546826cf02b360a2988dbc2d
Bryan Liff <bliff@minerva-group.com>
2017-10-10 22:21:23 -0400

    Add --no-merges to $ git log

--- git-log-local.sh
+++ git-log-local.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 branch=$(git rev-parse --abbrev-ref HEAD)
-git log "$branch" --not $(git for-each-ref --format='%(refname)' refs/heads/ | grep -v "refs/heads/$branch")
+git log "$branch" --no-merges --not $(git for-each-ref --format='%(refname)' refs/heads/ | grep -v "refs/heads/$branch")
 
