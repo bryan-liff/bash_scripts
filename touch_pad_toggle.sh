513d067d8b17a77ebed4aac717168f143f09ae03
Bryan Liff <bliff@minerva-group.com>
2022-04-16 14:42:05 -0400

    Script comments update and tmp txt removal

--- touch_pad_toggle.sh
+++ touch_pad_toggle.sh
@@ -1,15 +1,18 @@
 #!/bin/sh
+#
+# Find XINPUT_DEVICE_ID via:
+#
+#   xinput --list
+#
+# Update '--icon' paths as needed
 
 XINPUT_DEVICE_ID=13
 
 if [ $(xinput list-props $XINPUT_DEVICE_ID | grep 'Device Enabled' | gawk -F ':\t' '{ print $2 }') -eq 0 ]; then
   xinput enable $XINPUT_DEVICE_ID
   notify-send --icon=/usr/share/icons/Yaru/scalable/devices/input-touchpad-symbolic.svg "Enabled" "Your computer's touchpad is enabled."
-  #./Yaru/scalable/status/touchpad-disabled-symbolic.svg
-  #./Yaru/scalable/devices/input-touchpad-symbolic.svg
 else
   xinput disable $XINPUT_DEVICE_ID
   notify-send --icon=/usr/share/icons/Yaru/scalable/status/touchpad-disabled-symbolic.svg "Disabled" "Your computer's touchpad is disabled."
-  #Yaru/scalable/status/touchpad-disabled-symbolic.svg
 fi
 
